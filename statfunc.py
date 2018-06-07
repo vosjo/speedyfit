Fixed bug in statfunc so that scale is calculated in a weighted average instead of normal average. Added stat_chi2 for grids

statfunc.py
@@ -278,9 +278,7 @@ def stat_chi2(meas, e_meas, colors, syn, pars, **kwargs):
       ratio = (meas/syn)[~colors]
       weights = (meas/e_meas)[~colors]
       #-- weighted average and standard deviation
-      scale = np.average(ratio)
-      #scale = np.average(ratio,weights=weights)
-      
+      scale = np.average(ratio,weights=weights)
       e_scale = np.sqrt(np.dot(weights, (ratio-scale)**2)/weights.sum())
    else:
       scale,e_scale = 0,0
@@ -332,3 +330,85 @@ def stat_chi2(meas, e_meas, colors, syn, pars, **kwargs):
    return chisq.sum(axis=0), scale, e_scale
   
 
+def stat_chi2_multiple(meas, e_meas, colors, syn, pars, **kwargs):
+   """
+   Calculate Chi2 and compute angular diameter.
+   
+   Colors and absolute fluxes are used to compute the Chi2, only absolute
+   fluxes are used to compute angular diameter. If no absolute fluxes are
+   given, the angular diameter is set to 0.
+   
+   If constraints are given in the kwargs they are included in the Chi2
+   calculation. Accepted constrains are distance and q (mass ratio). Both
+   should be given as a (value, error) tuple.
+   
+   @param meas: array of measurements
+   @type meas: 1D array
+   @param e_meas: array containing measurements errors
+   @type e_meas: 1D array
+   @param colors: boolean array separating colors (True) from absolute fluxes (False)
+   @type colors: 1D boolean array
+   @param syn: synthetic fluxes and colors
+   @type syn: 1D array
+   @param full_output: set to True if you want individual chisq
+   @type full_output: boolean
+   @return: chi-square, scale, e_scale
+   @rtype: float,float,float
+   """
+   
+   # First deal with Chi2 of the observations
+   #=========================================
+   #-- if syn represents a grid of measurements
+   if sum(~colors) > 0:
+      ratio = (meas/syn)[~colors]
+      weights = (meas/e_meas)[~colors]
+      #-- weighted average and standard deviation
+      scale = np.average(ratio,weights=weights.reshape(-1),axis=0)
+      e_scale = np.sqrt(np.dot(weights.T, (ratio-scale)**2)/weights.sum(axis=0))[0]
+   else:
+      scale = np.zeros(syn.shape[1])
+   
+   #-- we don't need to scale the colors, only the absolute fluxes
+   chisq = np.where(colors.reshape(-1,1), (syn-meas)**2/e_meas**2, (syn*scale-meas)**2/e_meas**2)
+   
+   
+   # Then add Chi2 of derived properties as distance, mass ratio, ...
+   #=================================================================
+   derived_properties = kwargs.get('constraints_syn', {})
+   constraints = kwargs.get('constraints', {})
+   
+   #-- Chi2 of the distance measurement
+   #   this can only be done if absolute fluxes are included in the fit.
+   if 'distance' in constraints and sum(~colors) > 0:
+      syn_scale = 1./constraints['distance'][0]**2
+      syn_scale_e = 2. * constraints['distance'][1] / constraints['distance'][0]**3 
+      
+      chi2_d = (scale - syn_scale)**2 / syn_scale_e**2
+      
+      # append to chisq array
+      chisq = np.vstack([chisq, chi2_d])
+   
+   #-- Chi2 of the Mass-Ratio
+   #   q needs to be computed elsewhere and provided in the kwargs
+   
+   if 'q' in constraints and 'q' in derived_properties:
+      q, q_e = constraints['q'][0], constraints['q'][1]
+      syn_q = derived_properties['q']
+      
+      chi2_q = (q - syn_q)**2 / q_e**2
+      
+      # append to chisq array
+      chisq = np.vstack([chisq, chi2_q])
+   
+   #-- calculate Chi2 of any remaining constraints on the parameters
+   for con, val in constraints.items():
+      if con in pars:
+         c, c_e = val[0], val[1]
+         syn_c = pars[con]
+         
+         chi2_c = (c - syn_c)**2 / c_e**2
+         
+         # append to chisq array
+         chisq = np.vstack([chisq, chi2_c])
+   
+   return chisq.sum(axis=0), scale, e_scale