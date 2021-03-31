a1bbefec5c0508ebabd9b0fff2346b0230c29ae0
Joris Vos <joris.vos@solargis.com>
2021-03-31 08:59:08 +0200

    New version: 0.2.0 Features: - Added support for Gaia EDR3, both for parallaxes and photometry - Added automatic calculation of parallax zero point offset based on Lindgren 2020 - Added functionality to integrate model atmosphere grids - Included transmission curves for most common photometric systems in the package
    Other:
    - Some code refactoring
    - Added some examples in the code to integrate atomspheric model grids
    
    Documentation: Still a work in progress.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 ]
 setup(
     name="speedyfit",
-    version="0.1.4",
+    version="0.2.0",
     author="Joris Vos",
     author_email="joris.vos@uv.cl",
     description="MC approach to fit photometric SEDs",
